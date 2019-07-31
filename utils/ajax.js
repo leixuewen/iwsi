7d9560f36be1f592cb1db127b5d44f92235722d7
lxw <lei_aa00@163.com>
2019-07-31 16:56:11 +0800

    完善 ajax.js

--- utils/ajax.js
+++ utils/ajax.js
@@ -1,7 +1,7 @@
 const ajax = {
     defaults: {
         url: '',
-        async: false,
+        async: true,
         method: 'GET',
         baseURL: '',
         headers: {},
@@ -18,14 +18,13 @@ const ajax = {
     request(obj) {
         ajax.interceptors.request(obj);
         let xhr = new XMLHttpRequest(), config = {...ajax.defaults, ...obj},
-            params = {...obj.params}, parameter;
-        xhr.timeout = config.timeout;
-        xhr.ontimeout = () => xhr.abort();
-        for (let k of Object.keys(config.headers)) {
-            xhr.setRequestHeader(k, config.headers[k]);
+            params = {...obj.params}, parameter = '';
+        if(config.async){
+            xhr.timeout = config.timeout;
+            xhr.ontimeout = () => xhr.abort();
         }
         for (let k of Object.keys(config)) {
-            if (!ajax.defaults[k]) {
+            if (ajax.defaults[k] === undefined) {
                 params[k] = config[k];
             }
         }
@@ -33,47 +32,50 @@ const ajax = {
             parameter = JSON.stringify(params);
         } else {
             for (let p of Object.keys(params)) {
-                parameter = parameter + '&' + p + '=' + params[p];
+                parameter += '&' + p + '=' + params[p];
             }
-            if (config.method === 'GET') {
+            if (config.method === 'GET' && parameter) {
                 config.url = config.url.indexOf('?') >= 0 ? config.url + parameter : config.url += "?" + parameter.substring(1);
             } else {
                 parameter = parameter.substring(1);
             }
         }
         xhr.open(config.method, config.baseURL + config.url, config.async);
+        for (let k of Object.keys(config.headers)) {
+            xhr.setRequestHeader(k, config.headers[k]);
+        }
         return new Promise((resolve, reject) => {
             xhr.onload = () => resolve(ajax.interceptors.response(xhr));
             xhr.onerror = () => reject(ajax.interceptors.response(xhr));
             xhr.send(parameter);
         });
     },
-    get(url, obj) {
+    get(url, obj = {}) {
         obj['url'] = url;
         obj['method'] = 'GET';
         return ajax.request(obj);
     },
-    delete(url, obj) {
+    delete(url, obj = {}) {
         obj['url'] = url;
         obj['method'] = 'DELETE';
         return ajax.request(obj);
     },
-    head(url, obj) {
+    head(url, obj = {}) {
         obj['url'] = url;
         obj['method'] = 'HEAD';
         return ajax.request(obj);
     },
-    post(url, obj) {
+    post(url, obj = {}) {
         obj['url'] = url;
         obj['method'] = 'POST';
         return ajax.request(obj);
     },
-    put(url, obj) {
+    put(url, obj = {}) {
         obj['url'] = url;
         obj['method'] = 'PUT';
         return ajax.request(obj);
     },
-    patch(url, obj) {
+    patch(url, obj = {}) {
         obj['url'] = url;
         obj['method'] = 'PATCH';
         return ajax.request(obj);
